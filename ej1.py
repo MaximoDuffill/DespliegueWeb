b9ea8cb1468fbe8756fdf7b984a1d3d9243f6cf5
Maximo <maximo@example.com>
2023-03-06 13:13:42 +0100

    If

--- ej1.py
+++ ej1.py
@@ -1,11 +1,18 @@
 nombre = input("Introduce tu nombre: ")
-
+valor = 8
 '''
-Comentario de yo que se
+Comentario conflicto
 '''
 
 def hola(nombre_recibido):
+    if valor < 4:
+        print("hola")
+    elif valor >= 4 valor < 9:
+        print("Adios")
+    else:
+        print("Buenos dias")
     print("Hola mundo " + nombre_recibido)
+    return 1
 
 hola(nombre)
 print("Fuera")
